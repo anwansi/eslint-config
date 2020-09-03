3f1ab21eec0394d9e6798a0a513deff6f79c14e5
Cedric Hobbs <cedric@anwansi.com>
2020-09-03 05:40:58 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,64 @@
+module.exports = {
+    'extends' : [
+        'plugin:vue/essential'
+    ],
+    rules : {
+        'indent' : ["error", 4, {
+            ignoreComments : true,
+            SwitchCase     : 1,
+            ignoredNodes   : ["ConditionalExpression"]
+        }],
+        'wrap-iife'   : "off",
+        'key-spacing' : ["error", {
+            singleLine : {
+                beforeColon : true,
+                afterColon  : true,
+                mode        : "strict"
+            },
+            multiLine : {
+                beforeColon : true,
+                afterColon  : true,
+                mode        : "minimum",
+                align       : "colon"
+            }
+        }],
+        'keyword-spacing' : ["error", {
+            overrides : {
+                "catch" : { after : false }
+            }
+        }],
+        'array-bracket-spacing'   : 'off',
+        'no-console'              : ['error', { allow : ["error", "warn"] }],
+        'no-debugger'             : 'error',
+        'no-multi-spaces'         : "off",
+        'no-multiple-empty-lines' : ["error", {
+            max    : 3,
+            maxEOF : 1,
+            maxBOF : 0
+        }],
+        'no-unneeded-ternary'         : 'off',
+        'no-unreachable'              : 'off',
+        'operator-linebreak'          : "off",
+        'quotes'                      : "off",
+        'semi'                        : ["error", "always"],
+        'space-before-function-paren' : ["error", "never"],
+        'space-unary-ops'             : "off",
+        'spaced-comment'              : 'off',
+        'yoda'                        : 'off'
+    },
+    overrides : [
+        {
+            files : [
+                '**/*.js'
+            ],
+            env : {
+                mocha : true
+            },
+            rules : {
+                'no-console'            : 'off',
+                'no-debugger'           : 'off',
+                'no-unused-expressions' : 'off'
+            }
+        }
+    ]
+};
